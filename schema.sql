cae4123786b2de2efa6fb8dbaeb8b778c72cca1e
Tom Pham <teepham16@gmail.com>
2023-04-25 18:33:11 -0700

    created SCHEMA

---
+++ schema.sql
@@ -0,0 +1,34 @@
+DROP SCHEMA IF EXISTS;
+CREATE SCHEMA market;
+
+CREATE TABLE market.youtube(
+    id VARCHAR(255) PRIMARY KEY,
+    youtube_date DATE,
+    Likes INT,
+    CommentCount INT,
+    ViewCount INT,
+    Title VARCHAR(255)
+    URL VARCHAR(255)
+);
+
+CREATE TABLE market.twitter(
+    id VARCHAR(255) PRIMARY KEY,
+    Likes INT,
+    Comments INT,
+    Reposts INT,
+    twitter_date DATE
+);
+
+CREATE TABLE market.instgram(
+    id VARCHAR(255) PRIMARY KEY,
+    hashtags VARCHAR(255),
+    commentsCount INT,
+    likesCount INT,
+    ig_timestamp timestamp,
+    videoViewCount INT,
+    videoPlayCount INT.
+    videoDuration INT    
+);
+
+-- Create TABLE market.facebook
+-- CREATE TABLE market.linkedin
